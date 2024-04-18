223a3a32ebf157b22015f35dcb57cabfc61932df
L2530L <elagrosa23A-0057@olopsc.edu.ph>
2024-04-18 19:44:08 +0800

    tinapos next card

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -7,7 +7,8 @@ import 'package:flip_card/flip_card.dart';
 class FlashCard extends StatelessWidget {
   final String frontText;
   final String backText;
-  const FlashCard({super.key, required this.frontText, required this.backText});
+
+  const FlashCard({super.key, required this.frontText, required this.backText,});
 
 //TEXT EDIT
 
@@ -15,26 +16,56 @@ class FlashCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: FlipCard(
+            
             // FRONT
             front: Containers(
-                child: Text(
-              frontText,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.black,
-              ),
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(40),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    child: Text(
+                      'Question',
+                      style: TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 75,
+                ),
+                Text(
+                  frontText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             )),
 
             // BACK
             back: Containers(
-                child: Text(
-              backText,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.black,
-              ),
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text('Answer'),
+                ),
+                Text(
+                  backText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
             ))));
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flash_card/components/card.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,19 +12,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int index = 0;
+  bool isFront = true;
+  var cardState = FlipCardState(true);
+
+
+
   List<FlashCard> deck = [
     FlashCard(
-      frontText: "dsadsa",
+      frontText: "1+0",
+      backText: "1",
+    ),
+    FlashCard(
+      frontText: "1+1",
       backText: "2",
     ),
     FlashCard(
-      frontText: "dsadsa",
-      backText: "2",
+      frontText: "2+1",
+      backText: "3",
     ),
-    FlashCard(
-      frontText: "dsadsa",
-      backText: "2",
-    ),
+
   ];
 
   @override
@@ -33,15 +41,26 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FlashCard(
-              frontText: "dsadsa",
-              backText: "2",
-            ),
+            deck[index],
+            
             SizedBox(
               height: 10,
             ),
+
             ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  if (index < deck.length-1) {
+                    setState(() {
+                      index += 1;
+                    });
+                  } else {
+                    setState(() {
+                      index = 0;
+                    });
+                  }
+                },
+
+
                 child: Text(
                   "Next Card",
                   style: TextStyle(

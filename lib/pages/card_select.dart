d4d5aa089d10a3071c97696de73800eecb7f6e13
L2530L <elagrosa23A-0057@olopsc.edu.ph>
2024-05-07 13:26:52 +0800

    Add card_select

---
+++ lib/pages/card_select.dart
@@ -0,0 +1,38 @@
+import 'package:flash_card/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GridView.count(
+      crossAxisCount: 1,
+      children: [
+        Container(
+          color: Colors.amber,
+          child: Material(
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const HomePage()),
+  );
+              },
+            ),
+          ),
+        ),
+        
+        //Container
+        Container(
+          color: Colors.black,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+      ],
+    ));
+  }
+}
